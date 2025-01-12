14ab3d17a6c424cf48b3f9f3ee741cd98452a8b5
serversaravanam22 <serversaravanam22@gmail.com>
2025-01-12 19:11:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,10 +12,10 @@ def is_enabled(value, default):
 
 
 # PyroClient Setup 
-API_ID = int(environ['20902603'])
-API_HASH = environ['79e5caa103a9e9fb0183390b4800845d']
-BOT_TOKEN = environ['7807123947:AAE0_FLfTir6_UmuUGDCDZ2LF-WknnHfx8k']
-
+SESSION = environ.get('SESSION', 'TechVJBot')
+API_ID = int(environ.get('API_ID', '20902603'))
+API_HASH = environ.get('API_HASH', '79e5caa103a9e9fb0183390b4800845d')
+BOT_TOKEN = environ.get('BOT_TOKEN', "7807123947:AAE0_FLfTir6_UmuUGDCDZ2LF-WknnHfx8k")
 # Bot settings
 WEB_SUPPORT = bool(environ.get("WEBHOOK", 'True')) # for web support on/off
 PICS = (environ.get('PICS' ,'https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg https://graph.org/file/a125497b6b85a1d774394.jpg https://graph.org/file/43d26c54d37f4afb830f7.jpg https://graph.org/file/60c1adffc7cc2015f771c.jpg https://graph.org/file/d7b520240b00b7f083a24.jpg https://graph.org/file/0f336b0402db3f2a20037.jpg https://graph.org/file/39cc4e15cad4519d8e932.jpg https://graph.org/file/d59a1108b1ed1c6c6c144.jpg https://te.legra.ph/file/3a4a79f8d5955e64cbb8e.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg')).split()
